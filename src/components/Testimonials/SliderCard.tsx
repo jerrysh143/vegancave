2db903c530c1d6b8662ded6e65ae5b97eb6b18f3
jay <jays4704.js@gmail.com>
2023-07-25 00:38:16 +0530

    Responsive

--- src/components/Testimonials/SliderCard.tsx
+++ src/components/Testimonials/SliderCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import UserImage from "../../images/AuthorImage.jpg"
+
 interface TestimonialProps {
   Description?: string;
   ImagePath?: string;
@@ -6,15 +8,18 @@ interface TestimonialProps {
   Position?: string;
 }
 
+const imgData = [{image: UserImage}]
+
 const SliderCard = (props: TestimonialProps) => {
   return (
     <div className=" p-0 m-15px sm:p-50px md:p-0 md:m-30px">
+      {imgData.map(e => 
       <div className="shadow-[0px_0px_50px_0px_rgba(0,0,0,0.10)] p-15px md:px-50px md:py-[100px] bg-white rounded-35px">
         <div className="flex items-center justify-center pb-20px md:pb-45px">
           <div className="mr-15px">
             <img
               className="w-75px h-75px rounded-full"
-              src={`"../../images/${props.ImagePath}"`}
+              src={e.image}
               alt=""
             />
           </div>
@@ -29,6 +34,7 @@ const SliderCard = (props: TestimonialProps) => {
           {props.Description}
         </p>
       </div>
+       )}
     </div>
   );
 };
