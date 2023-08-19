a53c5b990ccf89688913d9476352f68da8a27510
jay soni <jays4704.js@gmail.com>
2023-08-19 15:33:13 +0000

    responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -113,19 +113,17 @@ const Header = () => {
 
   return (
     <div
-      className={`z-[999] px-15px ${
-        scrollPosition > 50
+      className={`z-[999] px-15px ${scrollPosition > 50
           ? "fixed slide-in w-full py-10px shadow-sm bg-white/80 duration-300 transition-all"
           : "bg-white py-20px duration-300 transition-all"
-      }`}
+        }`}
     >
       <div className="w-full max-w-full 1600:w-[1530px] mx-auto">
-        <div className="flex flex-wrap -ml-15px -mr-15px justify-between items-center">
+        <div className="flex flex-wrap justify-between items-center">
           <div className="w-2/12 md:w-5/12 px-15px flex justify-start">
             <div
-              className={`block cursor-pointer z-[999] ${
-                Menu ? "opened absolute top-30px left-20px" : ""
-              }`}
+              className={`block cursor-pointer z-[999] ${Menu ? "opened absolute top-30px left-20px" : ""
+                }`}
               onClick={() => {
                 setSearch(false);
                 setMenu(!Menu);
@@ -141,11 +139,10 @@ const Header = () => {
               }}
             >
               <MobileMenu
-                className={`z-[99] flex fixed top-0 left-0 m-15px w-[calc(100%-30px)] 500:w-[calc(100%-50%)] md:w-[calc(100%-80%)] ${
-                  Menu
+                className={`z-[99] flex fixed top-0 left-0 m-15px w-[calc(100%-30px)] 500:w-[calc(100%-50%)] md:w-[calc(100%-80%)] ${Menu
                     ? "translate-x-0 opacity-100 duration-150 transition-all"
                     : "-translate-x-full opacity-0 duration-150 transition-all"
-                }`}
+                  }`}
               />
             </div>
           </div>
@@ -165,7 +162,7 @@ const Header = () => {
               <div className="">
                 <div className="rounded-full border-2 border-black">
                   <Link to={ROUTE_NAME.LOGIN}>
-                  <UserIcon class="border-2 rounded-full" />
+                    <UserIcon class="border-2 rounded-full" />
                   </Link>
                 </div>
               </div>
@@ -175,17 +172,17 @@ const Header = () => {
                 <CartIcon className="w-25px" />
               </Link>
             </div>
-            <div className="z-[99] fixed top-0 right-0 m-15px rounded-15px bg-white">
-            <div className="flex items-center justify-center px-15px py-50px">
-              <input
-                type="search"
-                placeholder="Search for..."
-                className="w-full"
-              />
-            </div>
+            <div className={`z-[99] fixed top-0 right-0 m-15px rounded-15px bg-white w-5/12 h-[calc(100dvh_-_30px)] ${Search ? "translate-x-0 opacity-100 duration-150 transition-all":"translate-x-full opacity-0 duration-150 transition-all"} `}>
+              <div className="flex items-center justify-center px-15px py-50px">
+                <input
+                  type="search"
+                  placeholder="Search for..."
+                  className="px-15px w-full h-[70px] text-26 border-b-2 border-black/30 focus:border-b-2 focus:border-black/30 focus:outline-0"
+                />
+              </div>
             </div>
             <div
-              className=""
+              className="z-[999]"
               onClick={() => {
                 setSearch(!Search);
                 setMenu(false);
@@ -193,7 +190,7 @@ const Header = () => {
               }}
             >
               {Search ? (
-                <CloseIcon className="w-30px block z-[9999]" />
+                <CloseIcon className="w-30px block fixed top-30px right-30px" />
               ) : (
                 <SearchIcon className="w-30px md:cursor-pointer xl:cursor-auto" />
               )}
